c9dfbf1e4db0ffa5aa06d5776b9b5d0e51fb224d
Abhiram13 <abhiramnagadi@gmail.com>
2024-01-15 12:48:19 +0530

    managing response headers and reading request

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 host = "127.0.0.1"
 port = 3002
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-   return "hello world!"
+   print(request.data)
+   response = make_response("Hello Client!");
+   response.headers['Content-Type'] = 'application/json'
+   return response;
 
 if __name__ == '__main__':
    app.run(debug=True,host=host, port=port)
